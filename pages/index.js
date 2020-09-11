b0318b75c19465c7626ae5c4869acc6c1356664d
joshuawootonn <joshuawootonn@gmail.com>
2020-09-11 06:30:31 -0500

    Initial commit from Create Next App

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Link href='/who-we-are'><a>Who we are</a></Link>
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
---
+++ pages/who-we-are.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const WhoWeAre = props => {
+
+    return (<div>Who we are!!
+        <Link href="/">
+            <a>Back to home</a>
+        </Link></div>);
+};
+
+export default WhoWeAre;
