8304ef416542e8f39c72192995869fe23e3b0178
joshuawootonn <joshuawootonn@gmail.com>
2020-09-12 09:42:53 -0500

    eslint wip

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended'
+    ],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    plugins: ['@typescript-eslint', 'react'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+
+    rules: {
+        'react/prop-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off'
+    },
+    overrides: [
+        {
+            files: ['public','src','pages'],
+            excludedFiles: ['node_modules', '.next','.storybook','styles'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 'off'
+            }
+        }
+    ]
+};
